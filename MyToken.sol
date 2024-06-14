4bc4b735faf642b69a4b4b97e93d45f7ffeae76b
Siddharth Shukla <61881540+gunner26735@users.noreply.github.com>
2024-06-14 15:25:35 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -8,10 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Laxmi is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("Laxmi", "LXM")
-        Ownable(initialOwner)
-        ERC20Permit("Laxmi")
+    constructor(address initialOwner) ERC20("Laxmi", "LXM") Ownable(initialOwner) ERC20Permit("Laxmi")
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
